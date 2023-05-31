7f5a6d61ce6ad3dbf379039b494212d11f12f269
Naydelin Zavala <123757421+naydelinzavala7@users.noreply.github.com>
2023-05-30 19:28:15 -0600

    Update header.php

--- header.php
+++ header.php
@@ -7,16 +7,23 @@
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
   <script src="https://kit.fontawesome.com/a552314827.js" crossorigin="anonymous"></script>
   <link rel="stylesheet" href="./public/css/style.css">
+  <link rel="stylesheet" href="estilos.css">
   <title>Comanda Cafetería</title>
 </head>
 <body>
 
-<nav class="navbar navbar-expand-lg navbar-dark ">
+<nav class="navbar navbar-expand-lg navbar-dark" style="height: 60px">
   <div class="container-fluid">
-    <h2 class="navbar-brand" href="#">Lunch-Line
-      <img src="./public/img/taza_logo.png" alt="" width="40" height="34" class="d-inline-block align-text-top">
-    </h2>
-    <h2 class="navbar-brand" href="#">CAJA</h2>
+    <h1 class="navbar-brand" href="#">Lunch-Line
+      <img src="./public/img/taza_logo.png" alt="" width="40" height="34" class="d-inline-block align-text-top" style="margin-top: -5px;">
+    </h1>
+    <h2 class="navbar-brand" href="#";>CAJA</h2>
+    <button id="logout" type="button" class="btn btn-primary" style="background-color: #DD540A; border-color: #DD540A">
+      <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-box-arrow-right" viewBox="0 0 25 25">
+        <path fill-rule="evenodd" d="M10 12.5a.5.5 0 0 1-.5.5h-8a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v2a.5.5 0 0 0 1 0v-2A1.5 1.5 0 0 0 9.5 2h-8A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h8a1.5 1.5 0 0 0 1.5-1.5v-2a.5.5 0 0 0-1 0v2z"/>
+        <path fill-rule="evenodd" d="M15.854 8.354a.5.5 0 0 0 0-.708l-3-3a.5.5 0 0 0-.708.708L14.293 7.5H5.5a.5.5 0 0 0 0 1h8.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3z"/>
+      </svg>
+    </button>
   </div>
 </nav>
 
