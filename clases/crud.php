1369b6bf4fcd6895f1faf3b153b10d4a16336bfa
EdmundoMedelDev <edramede@gmail.com>
2023-06-01 12:44:47 -0600

    no funciona eliminar aun

--- clases/crud.php
+++ clases/crud.php
@@ -22,6 +22,21 @@ class Crud extends Conexion{
     }
   }
 
+  public function eliminarPedido($id){
+    try {
+      $conexion = Conexion::conectar(); 
+      $coleccion = $conexion->pedidos;
+      $respuesta = $coleccion->deleteOne(
+                    array(
+                      '_id'=> new MongoDB\BSON\ObjectId($id)
+                    )
+                  );
+      return $respuesta;
+    } catch (\Throwable $th) {
+      return $th->getMessage();
+    }
+  }
+
 }
 
 ?>
--- pedidos.php
+++ pedidos.php
@@ -23,8 +23,6 @@
                             </tr>
                         </thead>
                         <tbody>
-                        <?php //print_r($datos);
-                        ?>
                         <?php
                             foreach($datos as $item) {
                         ?>
@@ -34,18 +32,14 @@
                                 <td> <?php echo $item->descripcion?> </td>
                                 <th> <?php echo $item->fecha?></th>
                                 <th>
-                                    <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#EditModal" style="background-color: #403e3d; margin-top: 10px; border-color: #403e3d">
-                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
-                                            <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
-                                            <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/>
-                                        </svg>
+                                    <button type="button" class="btn btn-primary" data-id="<?php echo $item->_id?>" data-bs-toggle="modal" data-bs-target="#EditModal" style="background-color: #403e3d; margin-top: 10px; border-color: #403e3d">
+                                      <i class="fa-solid fa-pen-to-square"></i>
                                     </button>
                                 </th>
                                 <th style="width: 111px">
-                                    <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target='#AdvertenciaModal' style="background-color: #ff0202; margin-top: 10px; border-color: #ff0202">
-                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash-fill" viewBox="0 0 16 16">
-                                            <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z"/>
-                                        </svg>
+                                    <button type="button" class="btn btn-primary eliminar-pedido" data-bs-toggle="modal" data-bs-target="#AdvertenciaModal" style="background-color: #ff0202; margin-top: 10px; border-color: #ff0202">
+                                      <input type="text" value="<?php echo $item->_id?>" id="itemID">
+                                      <i class="fa-sharp fa-solid fa-trash"></i>
                                     </button>
                                 </th>
                             </tr>
@@ -71,9 +65,7 @@
                                 <td> Ya no hay coca </td>
                                 <td>
                                     <button type="button" class="btn btn-primary"data-bs-toggle="modal" data-bs-target="#AdveMensModal" style="background-color: #ff0202; margin-top: 10px;">
-                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash-fill" viewBox="0 0 16 16">
-                                            <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z"/>
-                                        </svg>
+                                      <i class="fa-sharp fa-solid fa-trash"></i>
                                     </button>
                                 </td>
                             </tr>
@@ -121,7 +113,7 @@
                             <button type="button" class="btn-close" data-bs-dismiss="modal" arial-label="Close"></button>
                         </div>
                         <div class="modal-body">
-                            <form action="./procesos/insertar.php" method="post">
+                            <form action="./procesos/insertarPedido.php" method="post">
                                 <div class="form-group">
                                     <label for="producto">Producto</label>
                                     <input type="text" class="form-control" id="producto" name="producto" aria-describedby="emailHelp" placeholder="Inserta el producto a comprar" required>
@@ -151,13 +143,16 @@
                             <h3 class="modal-title" id="modalTitle" style="color: white; padding-left: 145px">¡Advertencia!</h3>
                             <button type="button" class="btn-close" data-bs-dismiss="modal" arial-label="Close"></button>
                         </div>
-                        <div style="margin-top: 25px">
-                            <h5> El pedido será eliminado</h5>
-                            <h5> ¿Desea Continuar?</h5>
-                        </div>
-                        <div class="modal-body">
-                            <button type="submit" class="btn btn-primary" style="background-color: #4f7115; margin-left: 0px; margin-top: 10px; width:83px; border-color: #4f7115">Eliminar</button>
-                        </div>
+                        <form action="./procesos/eliminarPedido.php" method="post">
+                            <div style="margin-top: 25px">
+                                <h5> El pedido será eliminado</h5>
+                                <h5> ¿Desea Continuar?</h5>
+                            </div>
+                            <div class="modal-body">
+                              <input type="text" value="<?php echo $item->_id?>" name="itemID">
+                              <button type="submit" class="btn btn-primary" style="background-color: #4f7115; margin-left: 0px; margin-top: 10px; width:83px; border-color: #4f7115">Eliminar</button>
+                            </div>
+                        </form>
                     </div>
                 </div>
             </div>
